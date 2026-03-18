940fd05e8a221641a7988ce80de8287db566ea20
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2026-03-18 14:59:54 -0300

    feat(layout): implement dynamic favicon loading
    This commit introduces a system for dynamically loading favicons, centralizing their management and improving maintainability.
    
    - The ``ds-web-flavor.ts`` file is updated to define all favicon assets, including their paths.
    - The root layout (``layout.tsx``) now imports this configuration and dynamically generates the appropriate `<link>` tags in the document `<head>`.
    - The static `metadata` object has been removed in favor of this dynamic approach, ensuring that all favicons are loaded consistently across the application.

---
+++ src/flavor/ds-web-flavor.ts
@@ -0,0 +1,17 @@
+export const basePathAssets = '/ds-web-flavor/assets'
+export const siteWwebmanifest = `${basePathAssets}/site.webmanifest.json`
+export const assets = {
+    favicon_io: [
+        { name: 'android-chrome-192x192.png', path: `${basePathAssets}/favicon_io/android-chrome-192x192.png` },
+        { name: 'android-chrome-512x512.png', path: `${basePathAssets}/favicon_io/android-chrome-512x512.png` },
+        { name: 'apple-touch-icon.png', path: `${basePathAssets}/favicon_io/apple-touch-icon.png` },
+        { name: 'favicon-16x16.png', path: `${basePathAssets}/favicon_io/favicon-16x16.png` },
+        { name: 'favicon-32x32.png', path: `${basePathAssets}/favicon_io/favicon-32x32.png` },
+        { name: 'favicon.ico', path: `${basePathAssets}/favicon_io/favicon.ico` },
+        { name: 'site.webmanifest', path: `${basePathAssets}/favicon_io/site.webmanifest` }
+    ],
+    logos: [
+        { name: 'ds-web-logo.jpg', path: `${basePathAssets}`, },
+        { name: 'logo.jpeg', path: `${basePathAssets}`, },
+    ]
+}
