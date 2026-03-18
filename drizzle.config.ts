65fcc68808501205178b871449b39e4564306bb1
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2026-03-18 12:04:23 -0300

    feat: remove dotenv configuration from drizzle and HeliconeCostAdapter files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,4 @@
-import dotEnv from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-dotEnv.config()
-console.log(process.env.DATABASE_URL)
 export default defineConfig({
     dialect: 'postgresql',
     schema: './src/tokenConsumption/infrastructure/db/schema.ts',
