7b22bfc810b7c0a28413468d620959529ccc55a1
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2026-03-18 15:52:08 -0300

    feat: update page title and description for Zapt AI AD GENERATOR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Zapt AI AD GENERATOR</title>
+        <meta name="description" content="Ad generation with Zapt AI" />
+        {assets.favicon_io.map((icon, index) => {
+          if (icon.name.includes('apple-touch-icon')) {
+            return <link key={index} rel="apple-touch-icon" href={icon.path} />;
+          } else if (icon.name.includes('.webmanifest')) {
+            return <link key={index} rel="manifest" href={icon.path} />;
+          } else if (icon.name.includes('.ico')) {
+            return <link key={index} rel="icon" href={icon.path} />;
+          } else if (icon.name.includes('.png')) {
+            const sizes = icon.name.split('-').pop()?.split('.')[0];
+            return <link key={index} rel="icon" type="image/png" sizes={sizes} href={icon.path} />;
+          }
+          return null;
+        })}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
