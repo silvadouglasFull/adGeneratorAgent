02406670fbb32b65f9b5bfc549bebe9d3d592b44
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2026-03-20 14:13:01 -0300

    feat: update user condition for UserAvatarWithTooltip in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default async function RootLayout({
       >
         <ThemeProvider>
           <div className="flex items-center justify-end gap-2 px-4 py-2">
-            {sessionInformation.userName && (
+            {sessionInformation.user && (
               <UserAvatarWithTooltip userName={sessionInformation.userName} />
             )}
             <ThemeToggle />
